2a8c8286f96997644dbe7ee9892c508c74deb9ef
r-kononov <rkononov@gochain.io>
2019-02-05 15:33:21 +0200

    testing release process

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ echo "Version: $version"
 
 make release
 
-tag="$version"
+tag="v$version"
 git add -u
 git commit -m "web3 CLI: $version release [skip ci]"
 
@@ -31,7 +31,7 @@ git push origin $tag
 
 # For GitHub
 url='https://api.github.com/repos/gochain-io/web3/releases'
-output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"$version\", \"name\": \"$version\"}" $url)
+output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"$version\", \"name\": \"v$version\"}" $url)
 upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload_url"]' | sed -E "s/\{.*//")
 html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
 curl --data-binary "@web3_linux"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=web3_linux >/dev/null
