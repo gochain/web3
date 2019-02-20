83441c90756a82f0f843fc0e713d90ecf3b02ebc
r-kononov <rkononov@gochain.io>
2019-02-20 20:50:20 +0200

    updated release script

--- release_mac.sh
+++ release_mac.sh
@@ -8,7 +8,7 @@ make release
 
 # Upload to github
 url='https://api.github.com/repos/gochain-io/web3/releases'
-output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"v$version\", \"name\": \"v$version\"}" $url)
+output=$(curl -s "$url/tags/v$version")
 upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload_url"]' | sed -E "s/\{.*//")
 html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
 curl --data-binary "@web3_mac"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=web3_mac >/dev/null
