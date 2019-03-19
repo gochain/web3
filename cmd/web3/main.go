f2af882fcb967e5fcd5477792b4fb10fe80e9f25
Travis Reeder <treeder@gmail.com>
2019-03-19 14:43:15 +0000

    todo comment

--- cmd/web3/main.go
+++ cmd/web3/main.go
@@ -1013,7 +1013,6 @@ func GenerateContract(ctx context.Context, contractType string, c *cli.Context) 
 			}
 			capped.Mul(capped, new(big.Int).Exp(big.NewInt(10), big.NewInt(int64(decimals)), nil))
 		}
-
 		params := assets.Erc20Params{
 			Symbol:    c.String("symbol"),
 			TokenName: c.String("name"),
@@ -1023,6 +1022,9 @@ func GenerateContract(ctx context.Context, contractType string, c *cli.Context) 
 			Burnable:  c.Bool("burnable"),
 			Decimals:  decimals,
 		}
+		// TODO: add initial-supply flag
+		// TODO: must have initial supply or be mintable, otherwise this is zero
+		// TODO: initial supply can be set in constructor given to owner, eg: _mint(msg.sender, initialSupply)
 		s, err := assets.GenERC20(ctx, &params)
 		if err != nil {
 			fatalExit(err)
