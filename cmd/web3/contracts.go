0f00043b1e2af2ba8758aa0eae8c123bb64c4773
Travis Reeder <treeder@gmail.com>
2019-05-13 13:33:52 -0400

    Some UX cleanup (#108)
    * Uses floats to interact instead of huge ints. Supports erc20 balances and transfers too
    
    * minor cleanup
    
    * Fix comments from @jmank88
    
    * Update web3.go

---
+++ cmd/web3/contracts.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/gochain-io/web3"
+)
+
+func ListContract(contractFile string) {
+
+	myabi := getAbi(contractFile)
+
+	switch format {
+	case "json":
+		fmt.Println(marshalJSON(myabi.Methods))
+		return
+	}
+
+	for _, method := range myabi.Methods {
+		fmt.Println(method)
+	}
+
+}
+
+func GetContractConst(ctx context.Context, rpcURL, contractAddress, contractFile, functionName string, parameters ...interface{}) (interface{}, error) {
+	client, err := web3.Dial(rpcURL)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to connect to %q: %v", rpcURL, err)
+	}
+	defer client.Close()
+	myabi := getAbi(contractFile)
+	if _, ok := myabi.Methods[functionName]; !ok {
+		return nil, fmt.Errorf("There is no such function: %v", functionName)
+	}
+	if !myabi.Methods[functionName].Const {
+		return nil, err
+	}
+	res, err := web3.CallConstantFunction(ctx, client, *myabi, contractAddress, functionName, parameters...)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot call the contract: %v", err)
+	}
+	return res, nil
+}
+
+func CallContract(ctx context.Context, rpcURL, privateKey, contractAddress, contractFile, functionName string,
+	amount int, waitForReceipt bool, parameters ...interface{}) {
+	client, err := web3.Dial(rpcURL)
+	if err != nil {
+		fatalExit(fmt.Errorf("Failed to connect to %q: %v", rpcURL, err))
+	}
+	defer client.Close()
+	myabi := getAbi(contractFile)
+	if _, ok := myabi.Methods[functionName]; !ok {
+		fmt.Println("There is no such function:", functionName)
+		return
+	}
+	if myabi.Methods[functionName].Const {
+		res, err := web3.CallConstantFunction(ctx, client, *myabi, contractAddress, functionName, parameters...)
+		if err != nil {
+			fatalExit(fmt.Errorf("Cannot call the contract: %v", err))
+		}
+		switch format {
+		case "json":
+			m := make(map[string]interface{})
+			m["response"] = res
+			fmt.Println(marshalJSON(m))
+			return
+		}
+		fmt.Println(res)
+		return
+	}
+	tx, err := web3.CallTransactFunction(ctx, client, *myabi, contractAddress, privateKey, functionName, amount, parameters...)
+	if err != nil {
+		fatalExit(fmt.Errorf("Cannot call the contract: %v", err))
+	}
+	if !waitForReceipt {
+		fmt.Println("Transaction address:", tx.Hash.Hex())
+		return
+	}
+	ctx, _ = context.WithTimeout(ctx, 10*time.Second)
+	receipt, err := web3.WaitForReceipt(ctx, client, tx.Hash)
+	if err != nil {
+		fatalExit(fmt.Errorf("Cannot get the receipt: %v", err))
+	}
+	printReceiptDetails(receipt, myabi)
+
+}
