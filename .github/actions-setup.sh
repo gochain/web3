5f9f80f069fd842b46d191f535e653d1c12f20a5
Travis Reeder <treeder@gmail.com>
2019-09-12 12:50:43 -0400

    wip

--- .github/actions-setup.sh
+++ .github/actions-setup.sh
@@ -6,3 +6,8 @@ cat <<- EOF > $HOME/.netrc
 		login $GITHUB_ACTOR
 		password $GITHUB_TOKEN
 EOF
+chmod 600 $HOME/.netrc
+
+  # Git requires our "name" and email address -- use GitHub handle
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config user.name "$GITHUB_ACTOR"
