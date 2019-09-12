5f3f5c7bd7c2a52b26ce895062bc36e716ddf643
Travis Reeder <treeder@gmail.com>
2019-09-12 11:35:45 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,24 +2,25 @@
 set -ex
 
 # ensure working dir is clean
-git status
-if [[ -z $(git status -s) ]]
-then
-  echo "tree is clean"
-else
-  echo "tree is dirty, please commit changes before running this"
-  exit 1
-fi
+# git status
+# if [[ -z $(git status -s) ]]
+# then
+#   echo "tree is clean"
+# else
+#   echo "tree is dirty, please commit changes before running this"
+#   exit 1
+# fi
 
-git pull
+# git pull
 
 version_file="cmd/web3/version.go"
-docker create -v /data --name file alpine /bin/true
-docker cp $version_file file:/data/version.go
+#docker create -v /data --name file alpine /bin/true
+#docker cp $version_file file:/data/version.go
 # Bump version, patch by default - also checks if previous commit message contains `[bump X]`, and if so, bumps the appropriate semver number - https://github.com/treeder/dockers/tree/master/bump
-docker run --rm -it --volumes-from file -w / treeder/bump --filename /data/version.go "$(git log -1 --pretty=%B)"
-docker cp file:/data/version.go $version_file
-version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
+lastcommitmsg=$(git log -1 --pretty=%B)
+version=$(docker run --rm -it -w / treeder/bump --filename $version_file $lastcommitmsg)
+# docker cp file:/data/version.go $version_file
+#version=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $version_file)
 echo "Version: $version"
 
 make release
@@ -29,8 +30,7 @@ git add -u
 git commit -m "web3 CLI: $version release [skip ci]"
 
 git tag -f -a $tag -m "version $version"
-git push
-git push origin $tag
+git push --follow-tags
 
 # For GitHub
 url='https://api.github.com/repos/gochain/web3/releases'
@@ -39,7 +39,6 @@ upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin
 html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
 curl --data-binary "@web3_linux"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=web3_linux >/dev/null
 curl --data-binary "@web3_alpine" -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=web3_alpine >/dev/null
-
 
 docker build -t gochain/web3:latest .
 docker tag gochain/web3:latest gochain/web3:$version
